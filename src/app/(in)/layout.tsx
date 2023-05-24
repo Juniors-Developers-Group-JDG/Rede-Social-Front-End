9b7126e0c9e81ce86c82dda97b0bc47e54097579
JamDev0 <jamdeveloper0@gmail.com>
2023-05-24 06:49:22 -0300

    Project initialization

---
+++ src/app/(in)/layout.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from 'react';
+
+interface InnerLayoutProps {
+  children: ReactNode;
+}
+
+export default function InnerLayout({ children }: InnerLayoutProps) {
+  return children;
+}
