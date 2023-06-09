51a9172efa04d3d7d8775689a21fc38f2bf53a32
Paulo Victor Capuano <pvcapuano@gmail.com>
2023-06-09 18:30:15 -0300

    style: fixing background color component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -10,12 +10,12 @@ const Input = ({ placeholder, icon }: InputProps) => {
     <div className="my-3 flex items-center justify-center">
       <label className="flex ">
         {icon && (
-          <i className="flex h-10 w-8 items-center justify-center rounded-l-md bg-jdg-50 text-jdg-500">
+          <i className="flex h-10 w-8 items-center justify-center rounded-l-md bg-jdg-component-bg text-jdg-500">
             {icon}
           </i>
         )}
         <input
-          className=" h-10 w-72 rounded-r-md bg-jdg-50 p-2 placeholder:text-left "
+          className=" h-10 w-72 rounded-r-md bg-jdg-component-bg p-2 placeholder:text-left "
           placeholder={placeholder}
         />
       </label>
