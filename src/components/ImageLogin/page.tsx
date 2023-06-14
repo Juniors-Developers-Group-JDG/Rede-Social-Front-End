d44d2e43ed1a2448dc6f6ec8a10b229ae56c74b4
Paulo Victor Capuano <pvcapuano@gmail.com>
2023-06-14 19:08:22 -0300

    add mobile screen

--- src/components/ImageLogin/page.tsx
+++ src/components/ImageLogin/page.tsx
@@ -5,14 +5,14 @@ import ImageLayout from '../../../assets/ImageLayout.svg';
 
 const ImageLogin = () => {
   return (
-    <div className="w-7/12 bg-jdg-500">
-      <div className="mt-28 flex h-20 w-3/4 items-center justify-center rounded-r-2xl bg-jdg-text">
-        <h1 className="text-[32px] font-semibold text-jdg-500">
+    <div className="hidden w-7/12 bg-jdg-500 lg:flex lg:flex-col">
+      <div className="mt-20 flex h-20 w-3/4 items-center justify-center rounded-r-2xl bg-jdg-text xl:mt-40">
+        <h1 className="text-3xl font-semibold text-jdg-500">
           Welcome to Website
         </h1>
       </div>
-      <div className="mt-4 flex w-3/4 flex-col items-center justify-center p-2">
-        <h2 className="mb-8 w-2/4 text-justify text-base text-jdg-text">
+      <div className="flex w-3/4 flex-col items-center justify-center p-2">
+        <h2 className="my-4 w-2/4 text-justify text-base text-jdg-text">
           It is a long established fact that a reader will be distracted by the
           readable content of a page when looking at its layout. The point of
           using Lorem Ipsum is that it has a more-or-less normal distribution of
