ac476bad910c105e89b99c278678066d585b7885
JamDev0 <jamdeveloper0@gmail.com>
2023-06-21 09:07:28 -0300

    fix: header input text size and icons size

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,19 +6,19 @@ import searchIcon from '../../assets/search.svg';
 
 export function Header() {
   return (
-    <header className="fixed bottom-0 flex w-screen bg-black-800 py-4 lg:sticky lg:top-0 lg:justify-between lg:bg-black-950 lg:px-44 lg:py-2">
+    <header className="lg:bg-black-960 fixed bottom-0 z-10 flex w-screen bg-black-800 py-4 lg:sticky lg:top-0 lg:justify-between lg:px-44 lg:py-2">
       <div className="hidden items-center lg:flex">
         <Image
           src={searchIcon}
           alt="icone de lupa"
-          className="h-5 w-5"
-          width={20}
-          height={20}
+          className="h-6 w-6"
+          width={24}
+          height={24}
         />
         <input
           type="text"
           placeholder="Pesquisar"
-          className="ml-3 bg-transparent text-blue-500 placeholder-blue-500"
+          className="ml-4 bg-transparent text-xl text-blue-500 placeholder-blue-500"
         />
       </div>
 
@@ -26,23 +26,23 @@ export function Header() {
         <Image
           src={bellIcon}
           alt="icone de sino"
-          className="h-5 w-5"
-          width={20}
-          height={20}
+          className="h-6 w-6"
+          width={24}
+          height={24}
         />
         <Image
           src={chatIcon}
           alt="icone de caixa de chat"
-          className="h-5 w-5"
-          width={20}
-          height={20}
+          className="h-6 w-6"
+          width={24}
+          height={24}
         />
         <Image
           src={searchIcon}
           alt="icone de lupa"
-          className="h-5 w-5 lg:hidden"
-          width={20}
-          height={20}
+          className="h-6 w-6 lg:hidden"
+          width={24}
+          height={24}
         />
       </div>
     </header>
