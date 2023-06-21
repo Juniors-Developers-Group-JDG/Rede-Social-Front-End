39e3162b4c1f7fe3090575f7cc418afcc5fa6f81
JamDev0 <jamdeveloper0@gmail.com>
2023-06-21 08:58:46 -0300

    fix: header layout

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,19 +6,44 @@ import searchIcon from '../../assets/search.svg';
 
 function Header() {
   return (
-    <header className="fixed bottom-0 flex h-[50px] w-screen bg-black-900 lg:top-0 lg:h-16 lg:flex-row lg:justify-between">
-      <div className="hidden pl-44 lg:flex">
-        <Image src={searchIcon} alt="icone de lupa" />
+    <header className="fixed bottom-0 flex w-screen bg-black-800 py-4 lg:sticky lg:top-0 lg:justify-between lg:bg-black-950 lg:px-44 lg:py-2">
+      <div className="hidden items-center lg:flex">
+        <Image
+          src={searchIcon}
+          alt="icone de lupa"
+          className="h-5 w-5"
+          width={20}
+          height={20}
+        />
         <input
           type="text"
           placeholder="Pesquisar"
-          className="ml-3 mt-[21px] h-5 bg-transparent text-blue-500 placeholder-blue-500"
+          className="ml-3 bg-transparent text-blue-500 placeholder-blue-500"
         />
       </div>
-      <div className="flex w-full flex-row justify-evenly lg:mr-[89px] lg:w-auto lg:gap-16 lg:border-l lg:border-r lg:border-blue-800 lg:pl-11 lg:pr-11">
-        <Image src={bellIcon} alt="icone de sino" />
-        <Image src={chatIcon} alt="icone de caixa de chat" />
-        <Image className="lg:hidden" src={searchIcon} alt="icone de lupa" />
+
+      <div className="flex w-full items-center justify-evenly lg:w-fit lg:gap-16 lg:border-x lg:border-blue-800 lg:px-10 lg:py-4">
+        <Image
+          src={bellIcon}
+          alt="icone de sino"
+          className="h-5 w-5"
+          width={20}
+          height={20}
+        />
+        <Image
+          src={chatIcon}
+          alt="icone de caixa de chat"
+          className="h-5 w-5"
+          width={20}
+          height={20}
+        />
+        <Image
+          src={searchIcon}
+          alt="icone de lupa"
+          className="h-5 w-5 lg:hidden"
+          width={20}
+          height={20}
+        />
       </div>
     </header>
   );
