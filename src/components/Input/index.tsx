47fee611bd979e5250a2571692aeceade6cc07a7
Paulo Victor Capuano <pvcapuano@gmail.com>
2023-06-27 11:25:03 -0300

    fix: fixing on focus input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -7,7 +7,7 @@ interface InputProps {
 
 const Input = ({ placeholder, icon }: InputProps) => {
   return (
-    <div className="my-3 flex items-center justify-center">
+    <div className="my-3 flex items-center justify-center ">
       <label className="flex ">
         {icon && (
           <i className="flex h-10 w-8 items-center justify-center rounded-l-md bg-jdg-component-bg text-jdg-500">
@@ -16,7 +16,7 @@ const Input = ({ placeholder, icon }: InputProps) => {
         )}
       </label>
       <input
-        className=" h-10 w-72 rounded-r-md bg-jdg-component-bg p-2 placeholder:text-left "
+        className=" h-10 w-72 rounded-r-md bg-jdg-component-bg p-2 outline-none placeholder:text-left focus:outline-none focus:ring focus:ring-jdg-500 "
         placeholder={placeholder}
       />
     </div>
