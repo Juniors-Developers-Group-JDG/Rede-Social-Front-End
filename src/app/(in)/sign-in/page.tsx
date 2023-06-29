0bc07d3abee4ed13fe2318cf96695df7068a919e
JamDev0 <jamdeveloper0@gmail.com>
2023-06-29 06:07:18 -0300

    removal: ImageLogin component from SignIn, as it is already being rendered because of the inner layout

--- src/app/(in)/sign-in/page.tsx
+++ src/app/(in)/sign-in/page.tsx
@@ -1,14 +1,7 @@
 import { FormLogin } from '@/components/FormLogin';
-import { ImageLogin } from '@/components/ImageLogin/page';
 
 const SignIn = () => {
-  return (
-    <div className="bg-jdg-bg flex">
-      <ImageLogin />
-
-      <FormLogin />
-    </div>
-  );
+  return <FormLogin />;
 };
 
 export default SignIn;
