7e50c8512e65931227c3bfaaeb75972c4d55d3a5
JamDev0 <jamdeveloper0@gmail.com>
2023-06-29 07:34:30 -0300

    feat: added every input props to Input component and removed my

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
-interface InputProps {
+interface InputProps
+  extends Omit<
+    React.DetailedHTMLProps<
+      React.InputHTMLAttributes<HTMLInputElement>,
+      HTMLInputElement
+    >,
+    'className'
+  > {
   icon?: React.ReactElement;
-  placeholder: string;
 }
 
-export const Input = ({ placeholder, icon }: InputProps) => {
+export const Input = ({ placeholder, icon, ...props }: InputProps) => {
   return (
-    <div className="my-3 flex h-10 w-full items-center justify-center rounded-md bg-component-bg focus-within:ring focus-within:ring-blue-500">
+    <div className="flex h-10 w-full items-center justify-center rounded-md bg-component-bg focus-within:ring focus-within:ring-blue-500">
       <label className="flex ">
         {icon && (
           <i className="flex w-8 items-center justify-center text-blue-500">
@@ -16,6 +22,7 @@ export const Input = ({ placeholder, icon }: InputProps) => {
         )}
       </label>
       <input
+        {...props}
         className="w-full bg-transparent p-2 text-text-black outline-none placeholder:text-text-black/60 focus:outline-none"
         placeholder={placeholder}
       />
