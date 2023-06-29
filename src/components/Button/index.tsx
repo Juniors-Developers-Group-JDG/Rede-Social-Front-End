5bdc5b3b6c0209290cec6e4893c4916407089371
JamDev0 <jamdeveloper0@gmail.com>
2023-06-29 08:16:48 -0300

    feat: added every button prop to component Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import { twMerge } from 'tailwind-merge';
 
-interface ButtonProps {
+interface ButtonProps
+  extends React.DetailedHTMLProps<
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > {
   children: string;
   icon?: React.ReactElement;
 }
-export const Button = ({ children, icon }: ButtonProps) => {
+export const Button = ({
+  children,
+  icon,
+  className,
+  ...props
+}: ButtonProps) => {
   return (
-    <button className="hover:children-blue-500 flex h-10 w-full items-center justify-center rounded-md bg-blue-500 p-2 font-bold hover:bg-component-bg">
+    <button
+      className={twMerge(
+        'flex h-10 w-full items-center justify-center rounded-md bg-blue-500 p-2 font-bold hover:bg-component-bg hover:text-blue-500',
+        className,
+      )}
+      {...props}
+    >
       {icon && <div className="mr-2">{icon}</div>}
       {children}
     </button>
