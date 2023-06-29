4d045f5dcdc841cf0af7c1f5d8c0d078dc68bbf9
JamDev0 <jamdeveloper0@gmail.com>
2023-06-29 06:07:43 -0300

    fix: app's colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,23 +9,6 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'jdg-50': '#EDF5FD',
-        'jdg-100': '#C8E0F9',
-        'jdg-200': '#7EB6F1',
-        'jdg-300': '#5AA1ED',
-        'jdg-400': '#358CE9',
-        'jdg-500': '#1877DC',
-        'jdg-600': '#1463B8',
-        'jdg-700': '#104F93',
-        'jdg-800': '#0C3C6E',
-        'jdg-900': '#061F37',
-        'jdg-component-bg': '#E4EEF1',
-        'jdg-container-bg': '#1B1F22',
-        'jdg-container2-bg': '#15181A',
-        'jdg-bg': '#0D0F10',
-        'jdg-text': '#D9D9D9',
-      },
-      colors: {
         black: {
           800: '#1B1F22',
           900: '#15181A',
@@ -47,6 +30,7 @@ module.exports = {
           black: '#111011',
           white: '#E7ECF1',
         },
+        'component-bg': '#E4EEF1',
       },
     },
   },
