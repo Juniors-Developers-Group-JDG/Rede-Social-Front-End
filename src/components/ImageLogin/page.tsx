dbecf27139ff71900861a779d551c344df39aba4
JamDev0 <jamdeveloper0@gmail.com>
2023-06-29 08:42:57 -0300

    fix: linting problems

--- src/components/ImageLogin/page.tsx
+++ src/components/ImageLogin/page.tsx
@@ -1,18 +1,17 @@
 import Image from 'next/image';
-import React from 'react';
 
 import ImageLayout from '../../../assets/ImageLayout.svg';
 
 export const ImageLogin = () => {
   return (
-    <div className="hidden w-7/12 bg-jdg-500 lg:flex lg:flex-col">
-      <div className="mt-20 flex h-20 w-3/4 items-center justify-center rounded-r-2xl bg-jdg-text xl:mt-40">
-        <h1 className="text-3xl font-semibold text-jdg-500">
+    <div className="bg-jdg-500 hidden w-7/12 lg:flex lg:flex-col">
+      <div className="bg-jdg-text mt-20 flex h-20 w-3/4 items-center justify-center rounded-r-2xl xl:mt-40">
+        <h1 className="text-jdg-500 text-3xl font-semibold">
           Welcome to Website
         </h1>
       </div>
       <div className="flex w-3/4 flex-col items-center justify-center p-2">
-        <h2 className="my-4 w-2/4 text-justify text-base text-jdg-text">
+        <h2 className="text-jdg-text my-4 w-2/4 text-justify text-base">
           It is a long established fact that a reader will be distracted by the
           readable content of a page when looking at its layout. The point of
           using Lorem Ipsum is that it has a more-or-less normal distribution of
