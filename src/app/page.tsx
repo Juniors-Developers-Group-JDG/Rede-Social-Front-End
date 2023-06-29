b106405e999dd24f6116d0b30ad01ebea2035f91
JamDev0 <jamdeveloper0@gmail.com>
2023-06-29 09:19:22 -0300

    feature: index page redirects to sign-in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,9 @@
-import Link from 'next/link';
+import { redirect } from 'next/navigation';
 
 const Home = () => {
-  return (
-    <main className="flex min-h-screen w-screen flex-col items-center px-16 py-12">
-      <h1 className="mb-6 text-xl">Home</h1>
+  redirect('/sign-in');
 
-      <ul className="flex list-none flex-col items-center gap-y-2">
-        <li>
-          <Link href="/sign-in" className="p-4">
-            Sign In
-          </Link>
-        </li>
-        <li>
-          <Link href="/sign-up" className="p-4">
-            Sign Up
-          </Link>
-        </li>
-
-        <li>
-          <Link href="/feed" className="p-4">
-            Feed
-          </Link>
-        </li>
-      </ul>
-    </main>
-  );
+  return <></>;
 };
 
 export default Home;
