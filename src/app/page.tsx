14e9a65cbc8b753a2fd71a3c1067f63ac9a4acd1
JamDev0 <jamdeveloper0@gmail.com>
2023-06-29 09:03:55 -0300

    fix: temporary home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,28 @@ import Link from 'next/link';
 
 const Home = () => {
   return (
-    <>
-      <h1>Home</h1>
-      <Link href="/" className="p-4">
-        Home
-      </Link>
-      <Link href="/sign-up" className="p-4">
-        SignUp
-      </Link>
-    </>
+    <main className="flex min-h-screen w-screen flex-col items-center px-16 py-12">
+      <h1 className="mb-6 text-xl">Home</h1>
+
+      <ul className="flex list-none flex-col items-center gap-y-2">
+        <li>
+          <Link href="/sign-in" className="p-4">
+            Sign In
+          </Link>
+        </li>
+        <li>
+          <Link href="/sign-up" className="p-4">
+            Sign Up
+          </Link>
+        </li>
+
+        <li>
+          <Link href="/feed" className="p-4">
+            Feed
+          </Link>
+        </li>
+      </ul>
+    </main>
   );
 };
 
