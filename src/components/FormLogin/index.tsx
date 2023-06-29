47fee59d5778fdef5c29aec188a7e38144028d51
JamDev0 <jamdeveloper0@gmail.com>
2023-06-29 07:02:43 -0300

    removed: not needed text color

--- src/components/FormLogin/index.tsx
+++ src/components/FormLogin/index.tsx
@@ -8,7 +8,7 @@ import { Input } from '../Input';
 export const FormLogin = () => {
   return (
     <form className="flex w-full flex-col items-center justify-center px-8 md:flex-[2]">
-      <h1 className="text-jdg-text mb-2 text-2xl">Login</h1>
+      <h1 className="mb-2 text-2xl">Login</h1>
       <Input
         placeholder={'Digite seu e-mail'}
         icon={<RiUserLine size={20} />}
@@ -19,10 +19,10 @@ export const FormLogin = () => {
       />
       <div className="mb-3 flex w-80 items-center justify-between">
         <Link href="/sign-up">
-          <span className="text-jdg-text text-sm">Esqueceu a senha?</span>
+          <span className="text-sm">Esqueceu a senha?</span>
         </Link>
         <Link href="/">
-          <span className="text-jdg-text text-sm">Não tem conta?</span>
+          <span className="text-sm">Não tem conta?</span>
         </Link>
       </div>
 
@@ -30,7 +30,7 @@ export const FormLogin = () => {
 
       <div className="my-3 inline-flex w-80 items-center justify-center">
         <hr className="bg-jdg-500 my-8 h-px w-36 border-0 "></hr>
-        <span className="text-jdg-text mx-2 text-sm">ou</span>
+        <span className="mx-2 text-sm">ou</span>
         <hr className="bg-jdg-500 my-8 h-px w-36 border-0 "></hr>
       </div>
 
