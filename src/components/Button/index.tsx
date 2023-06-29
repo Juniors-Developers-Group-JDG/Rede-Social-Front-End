b33857e2ed0c1bddfac26618ea59718fc6f774bc
JamDev0 <jamdeveloper0@gmail.com>
2023-06-29 08:06:32 -0300

    feat: switched text by children prop in Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 interface ButtonProps {
-  text: string;
+  children: string;
   icon?: React.ReactElement;
 }
-export const Button = ({ text, icon }: ButtonProps) => {
+export const Button = ({ children, icon }: ButtonProps) => {
   return (
-    <button className="flex h-10 w-full items-center justify-center rounded-md bg-blue-500 p-2 font-bold hover:bg-component-bg hover:text-blue-500">
+    <button className="hover:children-blue-500 flex h-10 w-full items-center justify-center rounded-md bg-blue-500 p-2 font-bold hover:bg-component-bg">
       {icon && <div className="mr-2">{icon}</div>}
-      {text}
+      {children}
     </button>
   );
 };
