b088f0e81db43bb0e0facc8f9928622b9bf710df
JamDev0 <jamdeveloper0@gmail.com>
2023-06-29 07:05:27 -0300

    Removed: not needed bracts

--- src/components/FormLogin/index.tsx
+++ src/components/FormLogin/index.tsx
@@ -9,12 +9,9 @@ export const FormLogin = () => {
   return (
     <form className="flex h-screen w-full flex-col items-center justify-center px-8 md:flex-[2]">
       <h1 className="mb-2 text-2xl">Login</h1>
+      <Input placeholder="Digite seu e-mail" icon={<RiUserLine size={20} />} />
       <Input
-        placeholder={'Digite seu e-mail'}
-        icon={<RiUserLine size={20} />}
-      />
-      <Input
-        placeholder={'Digite sua senha'}
+        placeholder="Digite sua senha"
         icon={<RiLockPasswordLine size={20} />}
       />
       <div className="mb-3 flex w-80 items-center justify-between">
@@ -26,7 +23,7 @@ export const FormLogin = () => {
         </Link>
       </div>
 
-      <Button text={'Entrar'} />
+      <Button text="Entrar" />
 
       <div className="my-3 inline-flex w-80 items-center justify-center">
         <hr className="bg-jdg-500 my-8 h-px w-36 border-0 "></hr>
@@ -34,7 +31,7 @@ export const FormLogin = () => {
         <hr className="bg-jdg-500 my-8 h-px w-36 border-0 "></hr>
       </div>
 
-      <Button text={'Entrar com o Google'} icon={<IoLogoGoogle size={20} />} />
+      <Button text="Entrar com o Google" icon={<IoLogoGoogle size={20} />} />
     </form>
   );
 };
