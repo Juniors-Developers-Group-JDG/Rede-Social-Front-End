a010b95c1961213d4a493043e6c2ac74a316bc8a
JamDev0 <jamdeveloper0@gmail.com>
2023-06-29 06:47:20 -0300

    Fix: layout datails

--- src/app/(in)/layout.tsx
+++ src/app/(in)/layout.tsx
@@ -10,13 +10,13 @@ interface InnerLayoutProps {
 export default function InnerLayout({ children }: InnerLayoutProps) {
   return (
     <section className="flex h-screen max-md:flex-col">
-      <div className="hidden flex-[3] flex-col items-start justify-center bg-[#1877DC] pr-4 text-center md:flex 2xl:items-center">
-        <div className="flex h-[70px] w-3/4 min-w-fit max-w-xl items-center justify-center rounded-r-[15px] bg-[#D9D9D9] px-4 2xl:rounded-[15px]">
-          <h1 className="text-[2rem] font-semibold text-[#1877DC]  max-lg:text-[1.75rem]">
+      <div className="hidden flex-[3] flex-col items-stretch justify-center bg-[#1877DC] pr-10 md:flex 2xl:items-center 2xl:pr-0">
+        <div className="flex h-[70px] w-full min-w-fit max-w-xl items-center justify-center rounded-r-[15px] bg-[#D9D9D9] px-4 2xl:rounded-[15px]">
+          <h1 className="text-[1.75rem] font-semibold text-[#1877DC]  lg:text-[2rem]">
             Welcome to Website
           </h1>
         </div>
-        <div className="ml-8 flex flex-col">
+        <div className="mx-8 flex w-fit flex-col 2xl:mx-0">
           <div className="mt-8 max-w-md text-start">
             <p className="text-base font-normal leading-5">
               It is a long established fact that a reader will be distracted by
@@ -26,12 +26,16 @@ export default function InnerLayout({ children }: InnerLayoutProps) {
               content here&apos;, making it look like readable English.
             </p>
           </div>
-          <Image
-            src={SignUpImage}
-            alt="Sign Up Image"
-            className="mt-7"
-            priority={true}
-          />
+          <div className="mt-7 w-full pr-12 ">
+            <Image
+              src={SignUpImage}
+              alt="Sign Up Image"
+              className="w-full max-w-md "
+              width={448}
+              height={299.797}
+              priority={true}
+            />
+          </div>
         </div>
       </div>
       {children}
