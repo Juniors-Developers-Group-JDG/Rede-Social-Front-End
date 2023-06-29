e5fd07ea684901ee328d3343ad57ccc1843e8b1f
JamDev0 <jamdeveloper0@gmail.com>
2023-06-29 07:19:47 -0300

    fix: Input layout and colors

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -7,16 +7,16 @@ interface InputProps {
 
 export const Input = ({ placeholder, icon }: InputProps) => {
   return (
-    <div className="my-3 flex items-center justify-center ">
+    <div className="my-3 flex h-10 w-full items-center justify-center rounded-md bg-component-bg focus-within:ring focus-within:ring-blue-500">
       <label className="flex ">
         {icon && (
-          <i className="flex h-10 w-8 items-center justify-center rounded-l-md bg-jdg-component-bg text-jdg-500">
+          <i className="flex w-8 items-center justify-center text-blue-500">
             {icon}
           </i>
         )}
       </label>
       <input
-        className=" h-10 w-72 rounded-r-md bg-jdg-component-bg p-2 outline-none placeholder:text-left focus:outline-none focus:ring focus:ring-jdg-500 "
+        className="w-full bg-transparent p-2 text-text-black outline-none placeholder:text-text-black/60 focus:outline-none"
         placeholder={placeholder}
       />
     </div>
