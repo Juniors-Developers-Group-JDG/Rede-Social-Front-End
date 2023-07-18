cdfb51ce26efe17129dc920f94537c063814cc99
Paulo Victor Capuano <pvcapuano@gmail.com>
2023-07-18 20:02:09 -0300

    feat: add request

--- src/components/FormLogin/index.tsx
+++ src/components/FormLogin/index.tsx
@@ -4,26 +4,62 @@ import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 
 import { GoogleLogo, Password, User } from '@/components/PhosphorIcons';
+import { fetcher } from '@/utils/swrFetcher';
 
 import { Button } from '../Button';
 import { Input } from '../Input';
+import { useState } from 'react';
 
 export const FormLogin = () => {
   const { push } = useRouter();
 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = event => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = event => {
+    setPassword(event.target.value);
+  };
+
+  async function handleFormSubmit(event) {
+    event.preventDefault();
+    const { user } = await fetch(
+      'https://back-social-media-production.up.railway.app/sessions',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      },
+    );
+
+    console.log(user);
+  }
+
   return (
     <div className="flex h-screen w-full items-center justify-center px-8 md:flex-[2]">
-      <form className="flex w-full max-w-xs flex-col items-center">
+      <form
+        className="flex w-full max-w-xs flex-col items-center"
+        onSubmit={handleFormSubmit}
+      >
         <h1 className="mb-2 text-2xl">Login</h1>
         <Input
           placeholder="Digite seu e-mail"
           icon={<User className="h-5 w-5" />}
           className="mb-4"
+          value={email}
+          onChange={handleEmailChange}
         />
         <Input
           placeholder="Digite sua senha"
           type="password"
           icon={<Password className="h-5 w-5" />}
+          value={password}
+          onChange={handlePasswordChange}
         />
         <div className="mb-3 flex w-full items-center justify-between gap-4 text-center">
           <Link href="/">
@@ -34,7 +70,7 @@ export const FormLogin = () => {
           </Link>
         </div>
 
-        <Button onClick={() => push('/feed')}>Entrar</Button>
+        <Button onClick={() => {}}>Entrar</Button>
 
         <div className="my-3 inline-flex w-full items-center justify-center">
           <hr className="my-8 h-px w-full border-0 bg-blue-500"></hr>
