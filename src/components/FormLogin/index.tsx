35b86c3a8fa919500ade07aabb9bbdcc80a20cc1
JAMDev <61752887+JamDev0@users.noreply.github.com>
2023-08-09 20:07:21 -0300

    Revert "Login"

--- src/components/FormLogin/index.tsx
+++ src/components/FormLogin/index.tsx
@@ -1,92 +1,29 @@
 'use client';
-import Cookies from 'js-cookie';
+
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-import { useState, useContext } from 'react';
 
 import { GoogleLogo, Password, User } from '@/components/PhosphorIcons';
-import { toastsContext } from '@/contexts/toasts';
 
 import { Button } from '../Button';
 import { Input } from '../Input';
 
 export const FormLogin = () => {
   const { push } = useRouter();
-  const context = useContext(toastsContext);
-
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(event.target.value);
-  };
-
-  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(event.target.value);
-  };
-
-  async function handleFormSubmit(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-
-    try {
-      const response = await fetch(
-        'https://social-media-back-end.up.railway.app/sessions',
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ email, password }),
-        },
-      );
-
-      if (response.ok) {
-        const { user } = await response.json();
-        Cookies.set('user', JSON.stringify(user));
-        Cookies.set('email', email);
-        push('/feed');
-      } else {
-        context.addToast({
-          title: 'Erro ao fazer o Login!',
-          type: 'error',
-          durationInMs: 5000,
-          closeButton: true,
-          content: 'Usuário ou senha incorretos.',
-        });
-        console.error('Login failed');
-      }
-    } catch (error) {
-      context.addToast({
-        title: 'Erro ao logar!',
-        type: 'error',
-        durationInMs: 5000,
-        closeButton: true,
-        content: 'Tente novamente.',
-      });
-      console.error('An error occurred:', error);
-    }
-  }
 
   return (
     <div className="flex h-screen w-full items-center justify-center px-8 md:flex-[2]">
-      <form
-        className="flex w-full max-w-xs flex-col items-center"
-        onSubmit={handleFormSubmit}
-      >
+      <form className="flex w-full max-w-xs flex-col items-center">
         <h1 className="mb-2 text-2xl">Login</h1>
         <Input
           placeholder="Digite seu e-mail"
           icon={<User className="h-5 w-5" />}
           className="mb-4"
-          value={email}
-          onChange={handleEmailChange}
         />
         <Input
           placeholder="Digite sua senha"
           type="password"
           icon={<Password className="h-5 w-5" />}
-          value={password}
-          onChange={handlePasswordChange}
         />
         <div className="mb-3 flex w-full items-center justify-between gap-4 text-center">
           <Link href="/forgot">
@@ -97,7 +34,7 @@ export const FormLogin = () => {
           </Link>
         </div>
 
-        <Button>Entrar</Button>
+        <Button onClick={() => push('/feed')}>Entrar</Button>
 
         <div className="my-3 inline-flex w-full items-center justify-center">
           <hr className="my-8 h-px w-full border-0 bg-blue-500"></hr>
