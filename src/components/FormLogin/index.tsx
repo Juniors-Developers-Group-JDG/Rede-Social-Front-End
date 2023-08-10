0e39d0e908d424daa2a0dbcf733e83e0bb3b1be4
Paulo Victor Capuano <pvcapuano@gmail.com>
2023-08-10 19:31:07 -0300

    feat: using create cookie from actions file

--- src/components/FormLogin/index.tsx
+++ src/components/FormLogin/index.tsx
@@ -2,28 +2,91 @@
 
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
+import { useState, useContext } from 'react';
 
+import { createCookie } from '@/app/actions';
 import { GoogleLogo, Password, User } from '@/components/PhosphorIcons';
+import { toastsContext } from '@/contexts/toasts';
 
 import { Button } from '../Button';
 import { Input } from '../Input';
 
 export const FormLogin = () => {
   const { push } = useRouter();
+  const context = useContext(toastsContext);
+
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  async function handleFormSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    try {
+      const response = await fetch(
+        'https://social-media-back-end.up.railway.app/sessions',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ email, password }),
+        },
+      );
+
+      if (response.ok) {
+        const { user } = await response.json();
+        await createCookie('user', JSON.stringify(user));
+        await createCookie('email', email);
+        push('/feed');
+      } else {
+        context.addToast({
+          title: 'Erro ao fazer o Login!',
+          type: 'error',
+          durationInMs: 5000,
+          closeButton: true,
+          content: 'Usuário ou senha incorretos.',
+        });
+        console.error('Login failed');
+      }
+    } catch (error) {
+      context.addToast({
+        title: 'Erro ao logar!',
+        type: 'error',
+        durationInMs: 5000,
+        closeButton: true,
+        content: 'Tente novamente.',
+      });
+      console.error('An error occurred:', error);
+    }
+  }
 
   return (
     <div className="flex h-screen w-full items-center justify-center px-8 md:flex-[2]">
-      <form className="flex w-full max-w-xs flex-col items-center">
+      <form
+        className="flex w-full max-w-xs flex-col items-center"
+        onSubmit={handleFormSubmit}
+      >
         <h1 className="mb-2 text-2xl">Login</h1>
         <Input
           placeholder="Digite seu e-mail"
           icon={<User className="h-5 w-5" />}
           className="mb-4"
+          value={email}
+          onChange={handleEmailChange}
         />
         <Input
           placeholder="Digite sua senha"
           type="password"
           icon={<Password className="h-5 w-5" />}
+          value={password}
+          onChange={handlePasswordChange}
         />
         <div className="mb-3 flex w-full items-center justify-between gap-4 text-center">
           <Link href="/forgot">
@@ -34,7 +97,7 @@ export const FormLogin = () => {
           </Link>
         </div>
 
-        <Button onClick={() => push('/feed')}>Entrar</Button>
+        <Button>Entrar</Button>
 
         <div className="my-3 inline-flex w-full items-center justify-center">
           <hr className="my-8 h-px w-full border-0 bg-blue-500"></hr>
@@ -42,10 +105,7 @@ export const FormLogin = () => {
           <hr className="my-8 h-px w-full border-0 bg-blue-500"></hr>
         </div>
 
-        <Button
-          icon={<GoogleLogo className="h-5 w-5" weight="bold" />}
-          onClick={() => push('/feed')}
-        >
+        <Button icon={<GoogleLogo className="h-5 w-5" weight="bold" />}>
           Entrar com o Google
         </Button>
       </form>
