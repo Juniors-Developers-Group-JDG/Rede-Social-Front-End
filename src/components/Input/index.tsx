086c15f19659486649b12ba55dd96b625508abc6
JamDev0 <jamdeveloper0@gmail.com>
2023-09-08 19:23:49 -0300

    feat: adaptation of input component to support react-hook-form

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -2,44 +2,41 @@ import classNames from 'classnames';
 import React from 'react';
 import { twMerge } from 'tailwind-merge';
 
-interface InputProps
-  extends React.DetailedHTMLProps<
-    React.InputHTMLAttributes<HTMLInputElement>,
-    HTMLInputElement
-  > {
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
   icon?: React.ReactElement;
   textCentered?: boolean;
 }
 
-export const Input = ({
-  placeholder,
-  icon,
-  className,
-  textCentered,
-  ...props
-}: InputProps) => {
-  return (
-    <div
-      className={twMerge(
-        'flex h-10 w-full items-center justify-center rounded-md bg-component-bg focus-within:ring focus-within:ring-blue-500',
-        className,
-      )}
-    >
-      <label className="flex ">
-        {icon && (
-          <i className="flex w-8 items-center justify-center text-blue-500">
-            {icon}
-          </i>
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ placeholder, icon, className, textCentered, ...props }, ref) => {
+    return (
+      <div
+        className={twMerge(
+          'flex h-10 w-full items-center justify-center rounded-md bg-component-bg focus-within:ring focus-within:ring-blue-500',
+          className,
         )}
-      </label>
-      <input
-        {...props}
-        className={classNames(
-          'w-full bg-transparent p-2 text-text-black outline-none placeholder:text-text-black/60 focus:outline-none',
-          { 'text-center': textCentered },
-        )}
-        placeholder={placeholder}
-      />
-    </div>
-  );
-};
+      >
+        <label className="flex ">
+          {icon && (
+            <i className="flex w-8 items-center justify-center text-blue-500">
+              {icon}
+            </i>
+          )}
+        </label>
+        <input
+          {...props}
+          className={classNames(
+            'w-full bg-transparent p-2 text-text-black outline-none placeholder:text-text-black/60 focus:outline-none',
+            { 'text-center': textCentered },
+          )}
+          placeholder={placeholder}
+          ref={ref}
+        />
+      </div>
+    );
+  },
+);
+Input.displayName = 'Input';
+
+export { Input };
