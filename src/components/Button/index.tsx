496cf50a740ffd17e2b4f470e2f55c2a490b5f4a
JamDev0 <jamdeveloper0@gmail.com>
2023-09-08 21:44:19 -0300

    feat: isProcessing button prop and its functionality

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,30 +1,53 @@
+import { CircleNotch } from 'phosphor-react-sc';
 import React from 'react';
 import { twMerge } from 'tailwind-merge';
 
-interface ButtonProps
-  extends React.DetailedHTMLProps<
-    React.ButtonHTMLAttributes<HTMLButtonElement>,
-    HTMLButtonElement
-  > {
-  children: string;
-  icon?: React.ReactElement;
-}
+type ButtonProps = React.DetailedHTMLProps<
+  React.ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+> &
+  (
+    | {
+        isProcessing: true;
+        disabled?: undefined;
+      }
+    | {
+        isProcessing?: undefined | false;
+        disabled?: boolean;
+      }
+  ) & {
+    children: string;
+    icon?: React.ReactElement;
+  };
 export const Button = ({
   children,
   icon,
   className,
+  disabled,
+  isProcessing,
   ...props
 }: ButtonProps) => {
   return (
     <button
       className={twMerge(
-        'flex h-10 w-full items-center justify-center rounded-md bg-blue-500 p-2 font-bold hover:bg-component-bg hover:text-blue-500',
+        'flex h-10 w-full items-center justify-center rounded-md bg-blue-500 p-2 font-bold [&:not([data-isProcessing="true"])]:hover:bg-component-bg [&:not([data-isProcessing="true"])]:hover:text-blue-500 [&[data-isProcessing="true"]]:cursor-wait [&[data-isProcessing="true"]]:opacity-70',
         className,
       )}
+      data-isProcessing={isProcessing}
+      disabled={isProcessing || disabled}
       {...props}
     >
-      {icon && <div className="mr-2">{icon}</div>}
-      {children}
+      {isProcessing ? (
+        <CircleNotch
+          weight="bold"
+          className="h-[1em] w-[1em] animate-[spin_850ms_ease-out_infinite]"
+        />
+      ) : (
+        <>
+          {icon && <div className="mr-2">{icon}</div>}
+          {children}
+        </>
+      )}
     </button>
   );
 };
