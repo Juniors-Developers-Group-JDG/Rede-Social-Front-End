d0a713f5181c064f742182c43847b18d47cef5da
JamDev0 <jamdeveloper0@gmail.com>
2023-11-07 02:06:20 -0300

    feat: added a innerLayout

--- src/app/(in)/layout.tsx
+++ src/app/(in)/layout.tsx
@@ -1,5 +1,9 @@
 import { redirect } from 'next/navigation';
 
+import { Aside } from '@/components/Aside';
+import { Header } from '@/components/Header';
+import { PostProvider } from '@/contexts/post';
+
 import { getCookie } from '../actions';
 
 interface InnerLayoutProps {
@@ -13,5 +17,15 @@ export default async function InnerLayout({ children }: InnerLayoutProps) {
     redirect('/sign-in');
   }
 
-  return <>{children}</>;
+  return (
+    <div className="flex max-h-screen min-h-screen flex-col overflow-y-hidden">
+      <PostProvider>
+        <Header />
+        <div className="flex flex-1 items-stretch lg:overflow-y-hidden">
+          <Aside />
+          {children}
+        </div>
+      </PostProvider>
+    </div>
+  );
 }
